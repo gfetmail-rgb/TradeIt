20924930ac99696b41f67f7c81ce785e3915877d
gfetmail-rgb <gfetmail@gmail.com>
2026-08-28 14:56:41 +0330

    Persist chart opening preference when settings are saved

--- Charts/ChartSettingsWindow.UserOptions.cs
+++ Charts/ChartSettingsWindow.UserOptions.cs
@@ -1,23 +1,39 @@
 using System;
 using System.Windows;
-using System.Windows.Controls;
 
 namespace TradeIt.Charts
 {
     public partial class ChartSettingsWindow
     {
+        private static readonly bool _userOptionSaveHandlerRegistered = RegisterUserOptionSaveHandler();
+
+        private static bool RegisterUserOptionSaveHandler()
+        {
+            EventManager.RegisterClassHandler(
+                typeof(ChartSettingsWindow),
+                System.Windows.Controls.Button.ClickEvent,
+                new RoutedEventHandler(UserOptionSaveHandler));
+            return true;
+        }
+
+        private static void UserOptionSaveHandler(object sender, RoutedEventArgs e)
+        {
+            if (sender is not System.Windows.Controls.Button button ||
+                button.Content?.ToString() != "ذخیره")
+                return;
+
+            if (Window.GetWindow(button) is ChartSettingsWindow window &&
+                window.OpenChartInNewTabCheckBox != null)
+            {
+                window.Settings.OpenChartInNewTab =
+                    window.OpenChartInNewTabCheckBox.IsChecked == true;
+            }
+        }
+
         protected override void OnContentRendered(EventArgs e)
         {
             base.OnContentRendered(e);
-
-            if (OpenChartInNewTabCheckBox != null)
-                OpenChartInNewTabCheckBox.IsChecked = Settings.OpenChartInNewTab;
-        }
-
-        private void SaveChartOpeningOption()
-        {
-            if (OpenChartInNewTabCheckBox != null)
-                Settings.OpenChartInNewTab = OpenChartInNewTabCheckBox.IsChecked == true;
+            OpenChartInNewTabCheckBox.IsChecked = Settings.OpenChartInNewTab;
         }
     }
 }
