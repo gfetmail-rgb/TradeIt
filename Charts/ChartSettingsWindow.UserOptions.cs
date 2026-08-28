3902420f860c0835f5d7a51132b5537348a1b726
gfetmail-rgb <gfetmail@gmail.com>
2026-08-28 14:57:17 +0330

    Correctly persist chart opening option on settings save

--- Charts/ChartSettingsWindow.UserOptions.cs
+++ Charts/ChartSettingsWindow.UserOptions.cs
@@ -1,20 +1,39 @@
 using System;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace TradeIt.Charts
 {
     public partial class ChartSettingsWindow
     {
+        private static readonly bool _saveHandlerRegistered = RegisterSaveHandler();
+
+        private static bool RegisterSaveHandler()
+        {
+            EventManager.RegisterClassHandler(
+                typeof(ChartSettingsWindow),
+                Button.ClickEvent,
+                new RoutedEventHandler(ChartSettingsSaveHandler),
+                true);
+            return true;
+        }
+
+        private static void ChartSettingsSaveHandler(object sender, RoutedEventArgs e)
+        {
+            if (sender is ChartSettingsWindow window &&
+                e.OriginalSource is Button button &&
+                button.Content?.ToString() == "ذخیره" &&
+                window.OpenChartInNewTabCheckBox != null)
+            {
+                window.Settings.OpenChartInNewTab =
+                    window.OpenChartInNewTabCheckBox.IsChecked == true;
+            }
+        }
+
         protected override void OnContentRendered(EventArgs e)
         {
             base.OnContentRendered(e);
             OpenChartInNewTabCheckBox.IsChecked = Settings.OpenChartInNewTab;
-        }
-
-        private void OpenChartInNewTab_Changed(object sender, RoutedEventArgs e)
-        {
-            if (OpenChartInNewTabCheckBox != null)
-                Settings.OpenChartInNewTab = OpenChartInNewTabCheckBox.IsChecked == true;
         }
     }
 }
