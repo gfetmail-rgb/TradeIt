da2c61833bf3f792401d5a90f3be138ff9d93c80
gfetmail-rgb <gfetmail@gmail.com>
2026-08-28 18:00:58 +0330

    Restore unified chart settings sync and crosshair application

--- Charts/ChartTabView.SettingsSync.cs
+++ Charts/ChartTabView.SettingsSync.cs
@@ -13,6 +13,12 @@ namespace TradeIt.Charts
                 typeof(ChartTabView),
                 FrameworkElement.LoadedEvent,
                 new RoutedEventHandler(ChartTabView_Loaded));
+
+            EventManager.RegisterClassHandler(
+                typeof(ChartTabView),
+                FrameworkElement.UnloadedEvent,
+                new RoutedEventHandler(ChartTabView_Unloaded));
+
             return true;
         }
 
@@ -23,7 +29,82 @@ namespace TradeIt.Charts
 
             ChartSettingsManager.SettingsChanged -= chart.ChartSettingsManager_SettingsChanged;
             ChartSettingsManager.SettingsChanged += chart.ChartSettingsManager_SettingsChanged;
-            chart.ApplyPersistedAppearance();
+
+            chart.ApplyStoredChartSettings();
+        }
+
+        private static void ChartTabView_Unloaded(object sender, RoutedEventArgs e)
+        {
+            if (sender is ChartTabView chart)
+                ChartSettingsManager.SettingsChanged -= chart.ChartSettingsManager_SettingsChanged;
+        }
+
+        private void ChartSettingsManager_SettingsChanged(object? sender, EventArgs e)
+        {
+            if (Dispatcher.CheckAccess())
+                ApplyStoredChartSettings();
+            else
+                Dispatcher.InvokeAsync(ApplyStoredChartSettings);
+        }
+
+        private void ApplyStoredChartSettings()
+        {
+            try
+            {
+                _settings = ChartSettingsManager.Current;
+                _gridVisible = _settings.GridVisible;
+
+                ApplyGridStyle(Chart);
+                ApplyGridStyle(VolumeChart);
+
+                ApplyCrosshairStyle();
+
+                Chart.Plot.FigureBackground.Color = ScottPlot.Color.FromHtml(_settings.FigureBackground);
+                Chart.Plot.DataBackground.Color = ScottPlot.Color.FromHtml(_settings.DataBackground);
+                Chart.Plot.Axes.Color(ScottPlot.Color.FromHtml(_settings.AxisColor));
+
+                VolumeChart.Plot.FigureBackground.Color = ScottPlot.Color.FromHtml(_settings.FigureBackground);
+                VolumeChart.Plot.DataBackground.Color = ScottPlot.Color.FromHtml(_settings.DataBackground);
+                VolumeChart.Plot.Axes.Color(ScottPlot.Color.FromHtml(_settings.AxisColor));
+
+                Chart.Refresh();
+                VolumeChart.Refresh();
+            }
+            catch
+            {
+                // A chart may be unloaded while settings are being changed.
+            }
+        }
+
+        private void ApplyGridStyle(ScottPlot.WPF.WpfPlot plot)
+        {
+            plot.Plot.Grid.IsVisible = _settings.GridVisible;
+            plot.Plot.Grid.LineColor = ScottPlot.Color.FromHtml(_settings.GridColor);
+            plot.Plot.Grid.LinePattern = ParseLinePattern(_settings.GridPattern);
+            plot.Plot.Grid.MajorLineWidth = (float)_settings.GridLineWidth;
+            plot.Plot.Grid.MinorLineWidth = (float)_settings.GridLineWidth;
+        }
+
+        private void ApplyCrosshairStyle()
+        {
+            if (_crosshair == null)
+                return;
+
+            _crosshair.LineColor = ScottPlot.Color.FromHtml(_settings.CrosshairColor);
+            _crosshair.LineWidth = (float)_settings.CrosshairLineWidth;
+            _crosshair.LinePattern = ParseLinePattern(_settings.CrosshairPattern);
+        }
+
+        private static ScottPlot.LinePattern ParseLinePattern(string? value)
+        {
+            return value switch
+            {
+                "Dotted" => ScottPlot.LinePattern.Dotted,
+                "Dashed" => ScottPlot.LinePattern.Dashed,
+                "DenselyDashed" => ScottPlot.LinePattern.DenselyDashed,
+                "DashDot" => ScottPlot.LinePattern.DashDot,
+                _ => ScottPlot.LinePattern.Solid
+            };
         }
     }
 }
