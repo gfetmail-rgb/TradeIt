b699fd786781ac9465f50fbb9cb150281244603a
gfetmail-rgb <gfetmail@gmail.com>
2026-08-28 17:59:14 +0330

    Remove duplicate settings changed handler

--- Charts/ChartTabView.SettingsSync.cs
+++ Charts/ChartTabView.SettingsSync.cs
@@ -23,59 +23,7 @@ namespace TradeIt.Charts
 
             ChartSettingsManager.SettingsChanged -= chart.ChartSettingsManager_SettingsChanged;
             ChartSettingsManager.SettingsChanged += chart.ChartSettingsManager_SettingsChanged;
-            chart.ApplyStoredChartSettings();
-        }
-
-        private void ChartSettingsManager_SettingsChanged(object? sender, EventArgs e)
-        {
-            ApplyStoredChartSettings();
-        }
-
-        private void ApplyStoredChartSettings()
-        {
-            _settings = ChartSettingsManager.Current;
-            _gridVisible = _settings.GridVisible;
-
-            ApplyGridStyle(Chart);
-            ApplyGridStyle(VolumeChart);
-            ApplyCrosshairStyle();
-
-            Chart.Refresh();
-            if (_volumeVisible)
-                VolumeChart.Refresh();
-        }
-
-        private void ApplyGridStyle(ScottPlot.WPF.WpfPlot plot)
-        {
-            plot.Plot.Grid.IsVisible = _gridVisible;
-            plot.Plot.Grid.MajorLineColor = ScottPlot.Color.FromHtml(_settings.GridColor);
-
-            var pattern = ParseLinePattern(_settings.GridPattern);
-            plot.Plot.Grid.XAxisStyle.MajorLineStyle.Pattern = pattern;
-            plot.Plot.Grid.YAxisStyle.MajorLineStyle.Pattern = pattern;
-            plot.Plot.Grid.XAxisStyle.MajorLineStyle.Width = (float)_settings.GridLineWidth;
-            plot.Plot.Grid.YAxisStyle.MajorLineStyle.Width = (float)_settings.GridLineWidth;
-        }
-
-        private void ApplyCrosshairStyle()
-        {
-            if (_crosshair == null)
-                return;
-
-            _crosshair.LineColor = ScottPlot.Color.FromHtml(_settings.CrosshairColor);
-            _crosshair.LineWidth = (float)_settings.CrosshairLineWidth;
-            _crosshair.LinePattern = ParseLinePattern(_settings.CrosshairPattern);
-        }
-
-        private static ScottPlot.LinePattern ParseLinePattern(string? value)
-        {
-            return value switch
-            {
-                "Dotted" => ScottPlot.LinePattern.Dotted,
-                "Dashed" => ScottPlot.LinePattern.Dashed,
-                "DenselyDashed" => ScottPlot.LinePattern.DenselyDashed,
-                _ => ScottPlot.LinePattern.Solid
-            };
+            chart.ApplyPersistedAppearance();
         }
     }
 }
