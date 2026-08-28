ae5097a31927440a6a24e26444d5454de6a47220
gfetmail-rgb <gfetmail@gmail.com>
2026-08-28 14:57:09 +0330

    Persist chart opening option directly from its checkbox

--- Charts/ChartSettingsWindow.UserOptions.cs
+++ Charts/ChartSettingsWindow.UserOptions.cs
@@ -5,35 +5,16 @@ namespace TradeIt.Charts
 {
     public partial class ChartSettingsWindow
     {
-        private static readonly bool _userOptionSaveHandlerRegistered = RegisterUserOptionSaveHandler();
-
-        private static bool RegisterUserOptionSaveHandler()
-        {
-            EventManager.RegisterClassHandler(
-                typeof(ChartSettingsWindow),
-                System.Windows.Controls.Button.ClickEvent,
-                new RoutedEventHandler(UserOptionSaveHandler));
-            return true;
-        }
-
-        private static void UserOptionSaveHandler(object sender, RoutedEventArgs e)
-        {
-            if (sender is not System.Windows.Controls.Button button ||
-                button.Content?.ToString() != "ذخیره")
-                return;
-
-            if (Window.GetWindow(button) is ChartSettingsWindow window &&
-                window.OpenChartInNewTabCheckBox != null)
-            {
-                window.Settings.OpenChartInNewTab =
-                    window.OpenChartInNewTabCheckBox.IsChecked == true;
-            }
-        }
-
         protected override void OnContentRendered(EventArgs e)
         {
             base.OnContentRendered(e);
             OpenChartInNewTabCheckBox.IsChecked = Settings.OpenChartInNewTab;
+        }
+
+        private void OpenChartInNewTab_Changed(object sender, RoutedEventArgs e)
+        {
+            if (OpenChartInNewTabCheckBox != null)
+                Settings.OpenChartInNewTab = OpenChartInNewTabCheckBox.IsChecked == true;
         }
     }
 }
