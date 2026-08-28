19e3f91c7b175cb17566292471aed535eb9c11db
gfetmail-rgb <gfetmail@gmail.com>
2026-08-28 17:07:23 +0330

    Apply persisted grid and crosshair appearance to charts

---
+++ Charts/ChartTabView.UserOptions.cs
@@ -0,0 +1,83 @@
+using System.Windows;
+
+namespace TradeIt.Charts
+{
+    public partial class ChartTabView
+    {
+        private static readonly bool _settingsHandlerRegistered = RegisterSettingsHandler();
+
+        private static bool RegisterSettingsHandler()
+        {
+            EventManager.RegisterClassHandler(
+                typeof(ChartTabView),
+                FrameworkElement.LoadedEvent,
+                new RoutedEventHandler(ApplyUserChartSettings),
+                true);
+            return true;
+        }
+
+        private static void ApplyUserChartSettings(object sender, RoutedEventArgs e)
+        {
+            if (sender is not ChartTabView view)
+                return;
+
+            view.ApplyPersistedAppearance();
+        }
+
+        private void ApplyPersistedAppearance()
+        {
+            try
+            {
+                var settings = ChartSettingsManager.Current;
+
+                _settings = settings;
+                _gridVisible = settings.GridVisible;
+
+                ApplyGridAppearance(Chart);
+                ApplyGridAppearance(VolumeChart);
+
+                if (_crosshair != null)
+                {
+                    _crosshair.LineColor = ScottPlot.Color.FromHtml(settings.CrosshairColor);
+                    _crosshair.LineWidth = settings.CrosshairLineWidth;
+                    _crosshair.LinePattern = ParseLinePattern(settings.CrosshairPattern);
+                }
+
+                Chart.Plot.FigureBackground.Color = ScottPlot.Color.FromHtml(settings.FigureBackground);
+                Chart.Plot.DataBackground.Color = ScottPlot.Color.FromHtml(settings.DataBackground);
+                Chart.Plot.Axes.Color(ScottPlot.Color.FromHtml(settings.AxisColor));
+
+                VolumeChart.Plot.FigureBackground.Color = ScottPlot.Color.FromHtml(settings.FigureBackground);
+                VolumeChart.Plot.DataBackground.Color = ScottPlot.Color.FromHtml(settings.DataBackground);
+                VolumeChart.Plot.Axes.Color(ScottPlot.Color.FromHtml(settings.AxisColor));
+
+                Chart.Refresh();
+                VolumeChart.Refresh();
+            }
+            catch
+            {
+                // User preferences must never prevent a chart from opening.
+            }
+        }
+
+        private void ApplyGridAppearance(ScottPlot.WPF.WpfPlot chart)
+        {
+            var settings = _settings;
+            chart.Plot.Grid.IsVisible = settings.GridVisible;
+            chart.Plot.Grid.LineColor = ScottPlot.Color.FromHtml(settings.GridColor);
+            chart.Plot.Grid.LineWidth = settings.GridLineWidth;
+            chart.Plot.Grid.LinePattern = ParseLinePattern(settings.GridPattern);
+        }
+
+        private static ScottPlot.LinePattern ParseLinePattern(string? value)
+        {
+            return value?.ToLowerInvariant() switch
+            {
+                "dotted" => ScottPlot.LinePattern.Dotted,
+                "dashed" => ScottPlot.LinePattern.Dashed,
+                "denselydashed" => ScottPlot.LinePattern.DenselyDashed,
+                _ => ScottPlot.LinePattern.Solid
+            };
+        }
+    }
+}
