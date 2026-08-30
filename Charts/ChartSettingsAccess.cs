781a5e47178a509d7369b73946c98d5d6bc2fd63
gfetmail-rgb <gfetmail@gmail.com>
2026-08-30 18:05:35 +0330

    Expose chart settings to main toolbar

---
+++ Charts/ChartSettingsAccess.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace TradeIt.Charts
+{
+    public partial class ChartTabView
+    {
+        public void ShowSettings()
+        {
+            OpenSettings();
+        }
+    }
+}
