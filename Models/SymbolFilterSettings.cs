36ef0a57481becbc2ac6e390db696cc2973022f9
gfetmail-rgb <gfetmail@gmail.com>
2026-08-30 20:20:00 +0330

    Add traded-in-last-X-days filter setting

--- Models/SymbolFilterSettings.cs
+++ Models/SymbolFilterSettings.cs
@@ -40,42 +40,34 @@
     public class PriceFilter
     {
         public bool Enabled { get; set; }
-
         public PriceField Field { get; set; }
-
         public NumericComparison Comparison { get; set; }
-
         public int Days { get; set; } = 1;
     }
 
     public class SymbolFilterSettings
     {
-        public TradeStatusFilter TradeStatus { get; set; }
-            = TradeStatusFilter.All;
-
-        public SymbolNameFilter NameFilter { get; set; }
-            = SymbolNameFilter.All;
-
+        public TradeStatusFilter TradeStatus { get; set; } = TradeStatusFilter.All;
+        public SymbolNameFilter NameFilter { get; set; } = SymbolNameFilter.All;
         public string NameText { get; set; } = "";
 
         public bool DaysWithoutTradeEnabled { get; set; }
-
         public int DaysWithoutTrade { get; set; }
 
+        public bool DaysWithTradeEnabled { get; set; }
+        public int DaysWithTrade { get; set; }
+
         public bool VolumeFilterEnabled { get; set; }
-
         public int VolumeAverageDays { get; set; } = 20;
-
         public double VolumeMultiplier { get; set; } = 2.0;
 
-        public PriceFilter[] PriceFilters { get; set; }
-            = new PriceFilter[5]
-            {
-                new PriceFilter(),
-                new PriceFilter(),
-                new PriceFilter(),
-                new PriceFilter(),
-                new PriceFilter()
-            };
+        public PriceFilter[] PriceFilters { get; set; } = new PriceFilter[5]
+        {
+            new PriceFilter(),
+            new PriceFilter(),
+            new PriceFilter(),
+            new PriceFilter(),
+            new PriceFilter()
+        };
     }
 }
