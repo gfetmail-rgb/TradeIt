69ae43fb577e91e05e9980c48e2bfe3afe54616c
gfetmail-rgb <gfetmail@gmail.com>
2026-08-30 21:02:08 +0330

    Fix TextBox ambiguity in search compatibility

--- MainWindow.SymbolSearchCompatibility.cs
+++ MainWindow.SymbolSearchCompatibility.cs
@@ -1,11 +1,9 @@
-using System.Windows.Controls;
-
 namespace TradeIt
 {
     public partial class MainWindow
     {
         // Compatibility member for legacy code in MainWindow.xaml.cs.
         // The visual search box was intentionally removed from MainWindow.xaml.
-        private readonly TextBox SymbolSearchTextBox = new TextBox();
+        private readonly System.Windows.Controls.TextBox SymbolSearchTextBox = new System.Windows.Controls.TextBox();
     }
 }
