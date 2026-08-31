2f492ae55ba229d9c2d2619836f06505d6c10f86
gfetmail-rgb <gfetmail@gmail.com>
2026-08-31 19:34:33 +0330

    Start TradeIt explicitly in maximized window mode

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,6 +1,24 @@
-﻿namespace TradeIt
+using System.Windows;
+
+namespace TradeIt
 {
-    public partial class App : System.Windows.Application
+    public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Startup must always be the normal application window.
+            // Chart fullscreen is never entered automatically.
+            var mainWindow = new MainWindow
+            {
+                WindowStyle = WindowStyle.SingleBorderWindow,
+                ResizeMode = ResizeMode.CanResize,
+                WindowState = WindowState.Maximized
+            };
+
+            MainWindow = mainWindow;
+            mainWindow.Show();
+        }
     }
 }
