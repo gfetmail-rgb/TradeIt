51e6fc0223e9be8db3bc924fa873078287b5cba2
gfetmail-rgb <gfetmail@gmail.com>
2026-09-03 19:40:28 +0330

    Fix missing ChartDisplayType definition

---
+++ Charts/ChartDisplayType.cs
@@ -0,0 +1,12 @@
+namespace TradeIt.Charts
+{
+    /// <summary>
+    /// Chart rendering modes supported by ChartTabView.
+    /// </summary>
+    public enum ChartDisplayType
+    {
+        Candlestick,
+        Line,
+        Bar
+    }
+}
