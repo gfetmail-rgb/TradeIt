0e70216b0ddfb1962b2d9b66c5d30dacea298bee
gfetmail-rgb <gfetmail@gmail.com>
2026-09-03 19:44:14 +0330

    Restore ChartTabView core and isolate reset zoom handler

---
+++ Charts/ChartTabView.ResetZoom.cs
@@ -0,0 +1,20 @@
+using System.Windows;
+
+namespace TradeIt.Charts
+{
+    public partial class ChartTabView
+    {
+        private void ResetZoomButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!_hasInitialView) return;
+
+            Chart.Plot.Axes.SetLimits(
+                _initialXMin,
+                _initialXMax,
+                _initialYMin,
+                _initialYMax);
+
+            Chart.Refresh();
+        }
+    }
+}
