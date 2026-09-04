cc867bc760c6195ae495a44936f31c854adf9a3b
gfetmail-rgb <gfetmail@gmail.com>
2026-09-05 01:17:59 +0330

    Fix ambiguous Brushes reference in arrow drawing

--- Charts/ChartTabView.ArrowAndInitialViewFix.cs
+++ Charts/ChartTabView.ArrowAndInitialViewFix.cs
@@ -3,6 +3,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
+using Brushes = System.Windows.Media.Brushes;
 
 namespace TradeIt.Charts
 {
