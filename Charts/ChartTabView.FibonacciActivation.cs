c19e06ca47204f18c238d767d79ddd0beb099447
gfetmail-rgb <gfetmail@gmail.com>
2026-09-04 20:34:40 +0330

    Fix Fibonacci tool activation and active button state

---
+++ Charts/ChartTabView.FibonacciActivation.cs
@@ -0,0 +1,48 @@
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace TradeIt.Charts
+{
+    public partial class ChartTabView
+    {
+        private static readonly bool _fibonacciActivationRegistered = RegisterFibonacciActivationHandling();
+
+        private static bool RegisterFibonacciActivationHandling()
+        {
+            EventManager.RegisterClassHandler(
+                typeof(ChartTabView),
+                FrameworkElement.LoadedEvent,
+                new RoutedEventHandler(FibonacciActivation_Loaded));
+            return true;
+        }
+
+        private static void FibonacciActivation_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (sender is not ChartTabView chart)
+                return;
+
+            chart.AttachUnifiedDrawingInput();
+            chart.UpdateFibonacciButtonVisualState();
+        }
+
+        private void UpdateFibonacciButtonVisualState()
+        {
+            bool retracementActive = (int)_activeDrawingTool == UnifiedFibRetracement;
+            bool extensionActive = (int)_activeDrawingTool == UnifiedFibExtension;
+
+            SetDrawingToolButtonState(DrawingFibRetracementButton, retracementActive);
+            SetDrawingToolButtonState(DrawingFibExtensionButton, extensionActive);
+        }
+
+        private static void SetDrawingToolButtonState(Button button, bool active)
+        {
+            button.Opacity = active ? 1.0 : 0.55;
+            button.BorderThickness = active ? new Thickness(2) : new Thickness(1);
+            button.BorderBrush = active
+                ? Brushes.DodgerBlue
+                : SystemColors.ControlDarkBrush;
+            button.FontWeight = active ? FontWeights.Bold : FontWeights.Normal;
+        }
+    }
+}
