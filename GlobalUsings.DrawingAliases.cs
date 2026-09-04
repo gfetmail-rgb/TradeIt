948b26e74386062574f0295b5672807b14bf89b3
gfetmail-rgb <gfetmail@gmail.com>
2026-09-04 15:35:28 +0330

    Resolve WPF drawing event type ambiguities

---
+++ GlobalUsings.DrawingAliases.cs
@@ -0,0 +1,6 @@
+global using KeyEventHandler = System.Windows.Input.KeyEventHandler;
+global using MouseEventHandler = System.Windows.Input.MouseEventHandler;
+global using MouseButtonEventHandler = System.Windows.Input.MouseButtonEventHandler;
+global using MouseEventArgs = System.Windows.Input.MouseEventArgs;
+global using MouseButtonEventArgs = System.Windows.Input.MouseButtonEventArgs;
+global using System.Windows.Media.Media3D;
