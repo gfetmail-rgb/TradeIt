dc058b12a2e9aa7526ee215365824224b5c5f0b8
gfetmail-rgb <gfetmail@gmail.com>
2026-09-04 13:13:08 +0330

    Initialize drawing tools reliably on chart load

---
+++ Charts/ChartTabView.DrawingInitialization.cs
@@ -0,0 +1,27 @@
+using System.Windows;
+
+namespace TradeIt.Charts
+{
+    public partial class ChartTabView
+    {
+        private static readonly bool _drawingToolsRegistered = RegisterDrawingToolsHandling();
+
+        private static bool RegisterDrawingToolsHandling()
+        {
+            EventManager.RegisterClassHandler(
+                typeof(ChartTabView),
+                FrameworkElement.LoadedEvent,
+                new RoutedEventHandler(DrawingTools_Loaded));
+            return true;
+        }
+
+        private static void DrawingTools_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (sender is not ChartTabView chart)
+                return;
+
+            chart.InitializeTechnicalDrawingHandling();
+            chart.InitializeTextDrawingHandling();
+        }
+    }
+}
