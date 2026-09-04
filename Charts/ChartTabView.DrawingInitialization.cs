0c99252b07b2c810d94efc824b4515c48c767002
gfetmail-rgb <gfetmail@gmail.com>
2026-09-04 20:00:59 +0330

    Initialize drawing tool cursor handling

--- Charts/ChartTabView.DrawingInitialization.cs
+++ Charts/ChartTabView.DrawingInitialization.cs
@@ -24,6 +24,7 @@ namespace TradeIt.Charts
             chart.InitializeTextDrawingHandling();
             chart.AttachAdvancedDrawingTools();
             chart.AttachUnifiedDrawingInput();
+            chart.InitializeDrawingCursorHandling();
         }
     }
 }
