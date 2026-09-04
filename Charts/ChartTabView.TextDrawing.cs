e01940b2aaffa91014431af5828c4d26fcd4569d
gfetmail-rgb <gfetmail@gmail.com>
2026-09-04 13:12:40 +0330

    Finalize text drawing tool

--- Charts/ChartTabView.TextDrawing.cs
+++ Charts/ChartTabView.TextDrawing.cs
@@ -23,35 +23,24 @@ namespace TradeIt.Charts
         private bool _textDrawingActive;
         private bool _textDrawingEventsAttached;
 
-        private static readonly bool _textDrawingRegistered = RegisterTextDrawingHandling();
-
-        private static bool RegisterTextDrawingHandling()
+        private void InitializeTextDrawingHandling()
         {
-            EventManager.RegisterClassHandler(
-                typeof(ChartTabView),
-                FrameworkElement.LoadedEvent,
-                new RoutedEventHandler(TextDrawing_Loaded));
-            return true;
-        }
-
-        private static void TextDrawing_Loaded(object sender, RoutedEventArgs e)
-        {
-            if (sender is not ChartTabView chart || chart._textDrawingEventsAttached)
+            if (_textDrawingEventsAttached)
                 return;
 
-            chart._textDrawingEventsAttached = true;
-            chart.DrawingTextButton.Click += chart.DrawingTextButton_Click;
-
-            chart.Chart.AddHandler(
+            _textDrawingEventsAttached = true;
+            DrawingTextButton.Click += DrawingTextButton_Click;
+            Chart.AddHandler(
                 UIElement.PreviewMouseLeftButtonDownEvent,
-                new WpfMouseButtonEventHandler(chart.TextDrawing_MouseDown),
+                new WpfMouseButtonEventHandler(TextDrawing_MouseDown),
                 true);
         }
 
-        private void DrawingTextButton_Click(object sender, RoutedEventArgs e)
+        private void DrawingTextButton_Click(object? sender, RoutedEventArgs e)
         {
             _textDrawingActive = true;
             Chart.UserInputProcessor.IsEnabled = false;
+            Chart.ReleaseMouseCapture();
             Chart.Focus();
             ChartInfoTextBlock.Text = $"{_symbol.Symbol} | متن: محل درج متن را روی نمودار انتخاب کنید";
         }
@@ -92,7 +81,7 @@ namespace TradeIt.Charts
             {
                 Title = "درج متن روی نمودار",
                 Width = 420,
-                Height = 170,
+                Height = 180,
                 WindowStartupLocation = WindowStartupLocation.CenterOwner,
                 ResizeMode = ResizeMode.NoResize,
                 FlowDirection = FlowDirection.RightToLeft,
@@ -102,7 +91,7 @@ namespace TradeIt.Charts
             var textBox = new TextBox
             {
                 Margin = new Thickness(12),
-                Height = 45,
+                Height = 55,
                 VerticalContentAlignment = VerticalAlignment.Center,
                 AcceptsReturn = true,
                 TextWrapping = TextWrapping.Wrap,
@@ -115,7 +104,7 @@ namespace TradeIt.Charts
                 Width = 80,
                 Height = 30,
                 IsDefault = true,
-                Margin = new Thickness(0, 0, 8, 10)
+                Margin = new Thickness(4, 0, 4, 10)
             };
 
             var cancelButton = new Button
@@ -124,7 +113,7 @@ namespace TradeIt.Charts
                 Width = 80,
                 Height = 30,
                 IsCancel = true,
-                Margin = new Thickness(8, 0, 0, 10)
+                Margin = new Thickness(4, 0, 4, 10)
             };
 
             var buttons = new StackPanel
