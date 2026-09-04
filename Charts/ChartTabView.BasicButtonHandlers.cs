e4e1587f4fbc182e7da51bcd8b2b29dd763a5565
gfetmail-rgb <gfetmail@gmail.com>
2026-09-04 23:28:06 +0330

    Replace delete-all toolbar action with hide-all drawings

--- Charts/ChartTabView.BasicButtonHandlers.cs
+++ Charts/ChartTabView.BasicButtonHandlers.cs
@@ -4,6 +4,8 @@ namespace TradeIt.Charts
 {
     public partial class ChartTabView
     {
+        private bool _allDrawingsVisible = true;
+
         private void HideChartButton_Click(object sender, RoutedEventArgs e)
         {
             _chartVisible = !_chartVisible;
@@ -33,6 +35,59 @@ namespace TradeIt.Charts
             HideToolsButton.Content = _toolsVisible
                 ? "پنهان کردن ابزارهای تکنیکال"
                 : "نمایش ابزارهای تکنیکال";
+        }
+
+        private void HideAllDrawingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            _allDrawingsVisible = !_allDrawingsVisible;
+
+            foreach (var drawing in _trendLines)
+                if (drawing.PlotLine != null) drawing.PlotLine.IsVisible = _allDrawingsVisible;
+
+            foreach (var drawing in _horizontalLines)
+                if (drawing.PlotLine != null) drawing.PlotLine.IsVisible = _allDrawingsVisible;
+
+            foreach (var drawing in _verticalLines)
+                if (drawing.PlotLine != null) drawing.PlotLine.IsVisible = _allDrawingsVisible;
+
+            foreach (var drawing in _rays)
+                if (drawing.PlotLine != null) drawing.PlotLine.IsVisible = _allDrawingsVisible;
+
+            foreach (var drawing in _parallelChannels)
+            {
+                if (drawing.BaseLine != null) drawing.BaseLine.IsVisible = _allDrawingsVisible;
+                if (drawing.ParallelLine != null) drawing.ParallelLine.IsVisible = _allDrawingsVisible;
+            }
+
+            foreach (var drawing in _drawingRectangles)
+                foreach (var line in drawing.Lines)
+                    line.IsVisible = _allDrawingsVisible;
+
+            foreach (var drawing in _pitchforks)
+            {
+                if (drawing.MedianLine != null) drawing.MedianLine.IsVisible = _allDrawingsVisible;
+                if (drawing.UpperLine != null) drawing.UpperLine.IsVisible = _allDrawingsVisible;
+                if (drawing.LowerLine != null) drawing.LowerLine.IsVisible = _allDrawingsVisible;
+            }
+
+            foreach (var drawing in _fibonacciDrawings)
+                foreach (var line in drawing.Lines)
+                    line.IsVisible = _allDrawingsVisible;
+
+            foreach (var drawing in _textDrawings)
+                if (drawing.PlotText != null) drawing.PlotText.IsVisible = _allDrawingsVisible;
+
+            foreach (var handle in _drawingSelectionHandles)
+                handle.IsVisible = _allDrawingsVisible;
+
+            foreach (var overlay in _drawingSelectionOverlays)
+                overlay.IsVisible = _allDrawingsVisible;
+
+            if (_textSelectionHandle != null)
+                _textSelectionHandle.IsVisible = _allDrawingsVisible;
+
+            HideAllDrawingsButton.Content = _allDrawingsVisible ? "پنهان کردن همه" : "نمایش همه";
+            Chart.Refresh();
         }
 
         private void ZoomInButton_Click(object sender, RoutedEventArgs e)
