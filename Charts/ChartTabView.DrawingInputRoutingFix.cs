8a089cc922525b8c5c11b68551c1db988a4a9d5c
gfetmail-rgb <gfetmail@gmail.com>
2026-09-05 22:48:42 +0330

    Remove duplicate mouse routing for Fibonacci and arrow tools

--- Charts/ChartTabView.DrawingInputRoutingFix.cs
+++ Charts/ChartTabView.DrawingInputRoutingFix.cs
@@ -27,52 +27,6 @@ namespace TradeIt.Charts
         {
             if (_drawingInputRoutingFixAttached) return;
             _drawingInputRoutingFixAttached = true;
-            InputManager.Current.PreProcessInput += DrawingInputRoutingFix_PreProcessInput;
-        }
-
-        private void DrawingInputRoutingFix_PreProcessInput(object sender, PreProcessInputEventArgs e)
-        {
-            if (e.StagingItem.Input is MouseButtonEventArgs mouseButton &&
-                mouseButton.ChangedButton == MouseButton.Left &&
-                SourceBelongsToThisChart(mouseButton.OriginalSource as DependencyObject) &&
-                !mouseButton.Handled &&
-                !_textDrawingActive)
-            {
-                int tool = (int)_activeDrawingTool;
-
-                if (tool == 10 || IsUnifiedFibonacciActive() || IsAdvancedDrawingTool)
-                {
-                    if (tool == 10)
-                        ArrowDrawing_MouseDown(Chart, mouseButton);
-                    else if (IsUnifiedFibonacciActive())
-                        UnifiedFib_MouseDown(mouseButton);
-                    else
-                        AdvancedDrawing_MouseDown(Chart, mouseButton);
-
-                    mouseButton.Handled = true;
-                    return;
-                }
-            }
-
-            if (e.StagingItem.Input is System.Windows.Input.MouseEventArgs mouseMove &&
-                SourceBelongsToThisChart(mouseMove.OriginalSource as DependencyObject) &&
-                !mouseMove.Handled &&
-                !_textDrawingActive)
-            {
-                int tool = (int)_activeDrawingTool;
-
-                if (tool == 10 || IsUnifiedFibonacciActive() || IsAdvancedDrawingTool)
-                {
-                    if (tool == 10)
-                        ArrowDrawing_MouseMove(Chart, mouseMove);
-                    else if (IsUnifiedFibonacciActive())
-                        UnifiedFib_MouseMove(mouseMove);
-                    else
-                        AdvancedDrawing_MouseMove(Chart, mouseMove);
-
-                    mouseMove.Handled = true;
-                }
-            }
         }
     }
 }
